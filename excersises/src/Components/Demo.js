b3cf0f4004cc1d2426b621531579bfe8d065947a
abhi1580 <abhi1580>
2024-04-06 12:49:04 +0530

    first change

---
+++ excersises/src/Components/Demo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Demo = () => {
+  return (
+    <div>
+      <h1 style={{ color: "grey", backgroundColor: "cyan" }}>
+        Welcome to React buddy. Happy Hacking!!{" "}
+      </h1>
+    </div>
+  );
+};
+
+export default Demo;
